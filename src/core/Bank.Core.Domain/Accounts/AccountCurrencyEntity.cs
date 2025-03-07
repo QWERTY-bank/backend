6484090e66fbdcc2417e15d5d381cbefe0288a28
Maksim <125686261+kubavsop@users.noreply.github.com>
2025-03-07 18:45:11 +0700

    fix personal account creation

--- src/core/Bank.Core.Domain/Accounts/AccountCurrencyEntity.cs
+++ src/core/Bank.Core.Domain/Accounts/AccountCurrencyEntity.cs
@@ -6,6 +6,6 @@ namespace Bank.Core.Domain.Accounts;
 public class AccountCurrencyEntity : BaseEntity<long>
 {
     public required decimal Value { get; set; }
-    public required long AccountId { get; init; }
+    public long AccountId { get; init; }
     public required CurrencyCode Code { get; init; }
 }
