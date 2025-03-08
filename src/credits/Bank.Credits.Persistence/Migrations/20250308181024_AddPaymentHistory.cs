9188f95f3945692bc612f81b515823a1f4d4d824
vitaliy ivanof <ivitalij741@gmail.com>
2025-03-09 01:11:01 +0700

    Add migrations

---
+++ src/credits/Bank.Credits.Persistence/Migrations/20250308181024_AddPaymentHistory.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bank.Credits.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPaymentHistory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "PaymentsInfo_LastPayment",
+                schema: "bank_credits",
+                table: "Credits",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PaymentsInfo_Payment",
+                schema: "bank_credits",
+                table: "Credits",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Payments",
+                schema: "bank_credits",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Key = table.Column<Guid>(type: "uuid", nullable: false),
+                    AccountId = table.Column<long>(type: "bigint", nullable: false),
+                    PaymentAmount = table.Column<decimal>(type: "numeric", nullable: false),
+                    PaymentDateTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    PaymentStatus = table.Column<int>(type: "integer", nullable: false),
+                    Type = table.Column<int>(type: "integer", nullable: false),
+                    CreditId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Payments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Payments_Credits_CreditId",
+                        column: x => x.CreditId,
+                        principalSchema: "bank_credits",
+                        principalTable: "Credits",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payments_CreditId",
+                schema: "bank_credits",
+                table: "Payments",
+                column: "CreditId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Payments",
+                schema: "bank_credits");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentsInfo_LastPayment",
+                schema: "bank_credits",
+                table: "Credits");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentsInfo_Payment",
+                schema: "bank_credits",
+                table: "Credits");
+        }
+    }
+}
