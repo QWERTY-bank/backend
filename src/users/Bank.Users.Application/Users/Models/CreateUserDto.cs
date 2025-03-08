c5734329df24b0b90a16288f9b113cac2dfe9b90
vitaliy ivanof <ivitalij741@gmail.com>
2025-03-08 20:53:05 +0700

    Add user creating

--- src/users/Bank.Users.Application/Users/Models/CreateUserDto.cs
+++ src/users/Bank.Users.Application/Users/Models/CreateUserDto.cs
@@ -2,13 +2,12 @@
 
 namespace Bank.Users.Application.Users.Models
 {
-    public class UserDto
+    public class CreateUserDto
     {
-        public required Guid Id { get; init; }
         public required string FullName { get; init; }
         public required DateOnly Birthday { get; init; }
         public required GenderType Gender { get; init; }
         public required string Phone { get; init; }
-        public required bool IsBlocked { get; set; }
+        public required string Password { get; init; }
     }
 }
