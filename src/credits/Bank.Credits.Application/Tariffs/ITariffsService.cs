7e007b26987c323743177aeef678c1f90e310310
vitaliy ivanof <ivitalij741@gmail.com>
2025-03-09 16:48:52 +0700

    Add tariffDto to response

--- src/credits/Bank.Credits.Application/Tariffs/ITariffsService.cs
+++ src/credits/Bank.Credits.Application/Tariffs/ITariffsService.cs
@@ -7,7 +7,7 @@ namespace Bank.Credits.Application.Tariffs
     public interface ITariffsService
     {
         Task<ExecutionResult<IPagedList<TariffDto>>> GetTariffsAsync(TariffsFilters filters, int page, int pageSize);
-        Task<ExecutionResult> CreateTariffAsync(CreateTariffDto model);
+        Task<ExecutionResult<TariffDto>> CreateTariffAsync(CreateTariffDto model);
         Task<ExecutionResult> DeleteTariffAsync(Guid tariffId);
     }
 }
