2bf51d4c07c2337d8fd2e58a43e089380220c81c
z1all <120786249+z1all@users.noreply.github.com>
2025-04-06 19:16:36 +0700

    добавил стили на страницу входа

--- src/users/Bank.Users.AuthWeb/Models/LoginViewModel.cs
+++ src/users/Bank.Users.AuthWeb/Models/LoginViewModel.cs
@@ -4,11 +4,14 @@ namespace Bank.Users.AuthWeb.Models
 {
     public class LoginViewModel
     {
-        [Required]
+        [Required(ErrorMessage = "Обязательное поле")]
         public string Phone { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Обязательное поле")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
+        
+        [Required(ErrorMessage = "Значение ReturnUrl не установлено")]
         public string ReturnUrl { get; set; }
     }
 }
